204960d40e2a0961ccf775ad0f17ec2ddbd658b0
emre-unaldi <emree.unaldi@gmail.com>
2024-07-09 21:08:39 +0300

    Auth service entity layer created

---
+++ real-estate-microservices/auth-service/src/main/java/unaldi/authservice/entity/ERole.java
@@ -0,0 +1,14 @@
+package unaldi.authservice.entity;
+
+/**
+ * Copyright (c) 2024
+ * All rights reserved.
+ *
+ * @author Emre Ünaldı
+ * @since 2.07.2024
+ */
+public enum ERole {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN
+}
