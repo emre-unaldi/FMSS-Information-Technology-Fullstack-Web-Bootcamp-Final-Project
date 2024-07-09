665751475ddf7a3489d80d7e7cbea3a6e387b25b
emre-unaldi <emree.unaldi@gmail.com>
2024-07-09 20:27:39 +0300

    Discovery client service created

---
+++ real-estate-microservices/discovery-client-service/src/main/java/unaldi/discoveryclientservice/DiscoveryClientServiceApplication.java
@@ -0,0 +1,15 @@
+package unaldi.discoveryclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryClientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryClientServiceApplication.class, args);
+    }
+
+}
