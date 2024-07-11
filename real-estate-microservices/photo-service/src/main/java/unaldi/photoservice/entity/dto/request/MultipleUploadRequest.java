45af3d16e49d383cb26bd706fbc234862be74692
emre-unaldi <emree.unaldi@gmail.com>
2024-07-11 16:44:52 +0300

    Photo service service layer created

---
+++ real-estate-microservices/photo-service/src/main/java/unaldi/photoservice/entity/dto/request/MultipleUploadRequest.java
@@ -0,0 +1,20 @@
+package unaldi.photoservice.entity.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Copyright (c) 2024
+ * All rights reserved.
+ *
+ * @author Emre Ünaldı
+ * @since 11.07.2024
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MultipleUploadRequest {
+    private MultipartFile[] photos;
+}
--- real-estate-microservices/photo-service/src/main/java/unaldi/photoservice/repository/PhotoRepository.java
+++ real-estate-microservices/photo-service/src/main/java/unaldi/photoservice/repository/PhotoRepository.java
@@ -3,9 +3,8 @@ package unaldi.photoservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import unaldi.photoservice.entity.Photo;
-
-import java.util.UUID;
 
 /**
  * Copyright (c) 2024
@@ -14,9 +13,10 @@ import java.util.UUID;
  * @author Emre Ünaldı
  * @since 11.07.2024
  */
+@Repository
 public interface PhotoRepository extends JpaRepository<Photo, String> {
 
     @Query("SELECT CASE WHEN (SELECT COUNT(photo) FROM Photo photo WHERE photo.name = :name) > 0 THEN true ELSE false END")
-    Boolean findByName(@Param("name") String name);
+    Boolean existsByPhotoName(@Param("name") String name);
 
 }
