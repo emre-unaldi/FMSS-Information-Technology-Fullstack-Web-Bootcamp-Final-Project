a6e33f1750d5bdd79797414c57bf20a9db195c53
emre-unaldi <emree.unaldi@gmail.com>
2024-07-13 02:44:48 +0300

    Advert service api result structure added and service layer created

---
+++ real-estate-microservices/advert-service/src/main/java/unaldi/advertservice/service/AdvertService.java
@@ -0,0 +1,26 @@
+package unaldi.advertservice.service;
+
+import unaldi.advertservice.entity.dto.request.AdvertSaveRequest;
+import unaldi.advertservice.entity.dto.request.AdvertUpdateRequest;
+import unaldi.advertservice.entity.dto.response.AdvertResponse;
+import unaldi.advertservice.utils.result.DataResult;
+import unaldi.advertservice.utils.result.Result;
+
+import java.util.List;
+
+/**
+ * Copyright (c) 2024
+ * All rights reserved.
+ *
+ * @author Emre Ünaldı
+ * @since 13.07.2024
+ */
+public interface AdvertService {
+
+    DataResult<AdvertResponse> save(AdvertSaveRequest advertSaveRequest);
+    DataResult<AdvertResponse> update(AdvertUpdateRequest advertUpdateRequest);
+    DataResult<AdvertResponse> findById(Long advertId);
+    DataResult<List<AdvertResponse>> findAll();
+    Result deleteById(Long advertId);
+
+}
