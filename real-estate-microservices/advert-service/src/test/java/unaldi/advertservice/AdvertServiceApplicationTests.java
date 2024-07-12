b827068025c816ce17d497e78ddbd4a0ea10094a
emre-unaldi <emree.unaldi@gmail.com>
2024-07-13 00:41:26 +0300

    Advert service has been created. Database, Eureka and file based logging configurations added

---
+++ real-estate-microservices/advert-service/src/test/java/unaldi/advertservice/AdvertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package unaldi.advertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
