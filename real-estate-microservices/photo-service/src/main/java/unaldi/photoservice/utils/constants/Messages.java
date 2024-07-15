18ff9f3e126fceab3877c9f4c8d6d1374123a5b0
emre-unaldi <emree.unaldi@gmail.com>
2024-07-15 05:50:20 +0300

    Photo service RabbitMQ configuration added. Producer class was created for the log queue and global logging structure was provided

--- real-estate-microservices/photo-service/src/main/java/unaldi/photoservice/utils/constants/Messages.java
+++ real-estate-microservices/photo-service/src/main/java/unaldi/photoservice/utils/constants/Messages.java
@@ -17,6 +17,9 @@ public class Messages {
     public static  final String PHOTO_MULTIPLE_UPLOAD = "Multiple photo uploaded successfully";
     public static final String PHOTO_FOUND = "Photo fetched successfully";
     public static final String PHOTOS_LISTED = "Photos listed successfully";
+    public static final String PHOTO_IDS_LISTED = "Photos were successfully listed by their ids";
     public static final String PHOTO_DELETED = "Photo deleted successfully";
+    public static final String PHOTO_DOWNLOADED = "Photo downloaded successfully";
+    public static final String LOG_WRITE_QUEUE = "Log record written to queue";
 
 }
